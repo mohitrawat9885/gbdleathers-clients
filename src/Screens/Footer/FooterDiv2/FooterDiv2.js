e174df0711ef29be0a409f6f0cf3a0d4d0676ea6
Mohit <mohitrawat9885@gmail.com>
2021-12-02 19:46:54 +0530

    Last Footer div updated to new UI

--- src/Screens/Footer/FooterDiv2/FooterDiv2.js
+++ src/Screens/Footer/FooterDiv2/FooterDiv2.js
@@ -1,64 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import InstagramIcon from "@mui/icons-material/Instagram";
+
 import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import PinterestIcon from "@mui/icons-material/Pinterest";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import RedditIcon from "@mui/icons-material/Reddit";
-import SocialDistanceIcon from "@mui/icons-material/SocialDistance";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+import KeyboardArrowUpRoundedIcon from "@mui/icons-material/KeyboardArrowUpRounded";
+
 import "./FooterDiv2.css";
 export default function Footer() {
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   return (
     <>
       <div className="footer-div-2">
-        <div className="footer-copy-right">
-          <span>© 2022 GBD Leathers, All rights reserved.</span>
-        </div>
         <div className="footer-social-icons-div">
           <ul>
-            <li>
-              <Link to="/">
-                <InstagramIcon style={{ fontSize: 30 }} />
-              </Link>
-            </li>
-            <li>
+            <li id="footer-social-icon-1">
               <Link to="/">
                 <FacebookIcon style={{ fontSize: 30 }} />
               </Link>
             </li>
-            <li>
-              <Link to="/">
-                <TwitterIcon style={{ fontSize: 30 }} />
-              </Link>
-            </li>
-            <li>
+
+            <li id="footer-social-icon-2">
               <Link to="/">
                 <LinkedInIcon style={{ fontSize: 30 }} />
               </Link>
             </li>
-            <li>
+            <li id="footer-social-icon-3">
               <Link to="/">
-                <PinterestIcon style={{ fontSize: 30 }} />
+                <TwitterIcon style={{ fontSize: 30 }} />
               </Link>
             </li>
-            <li>
+
+            <li id="footer-social-icon-4">
               <Link to="/">
-                <YouTubeIcon style={{ fontSize: 30 }} />
+                <InstagramIcon style={{ fontSize: 30 }} />
               </Link>
             </li>
-            {/* <li>
-              <Link to="/">
-                <RedditIcon style={{ fontSize: 30 }} />
-              </Link>
-            </li> */}
-            {/* <li>
-              <Link to="/">
-                <SocialDistanceIcon style={{ fontSize: 30 }} />
-              </Link>
-            </li> */}
           </ul>
+          <div className="footer-gotoup-btn" onClick={() => topFunction()}>
+            <KeyboardArrowUpRoundedIcon
+              style={{
+                fontSize: 45,
+                // color: "white",
+              }}
+            />
+          </div>
+        </div>
+        <div className="footer-copy-right">
+          <span>© 2022 GBD Leathers, All rights reserved.</span>
         </div>
       </div>
     </>
